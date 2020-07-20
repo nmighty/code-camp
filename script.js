ebe6aa921ae30c54eee77955d62b784aaceefb74
nmighty <nicholas.mighty@gmail.com>
2020-07-20 18:16:36 -0400

    Added JavaScript function

---
+++ script.js
@@ -0,0 +1,5 @@
+function change()
+{
+alert('Sorry, Minecraft Dungeons is not available right now. Please leave a message at the tone. BEEP!');
+document.getElementById('changamathingie').style.backgroundColor='white';
+}
